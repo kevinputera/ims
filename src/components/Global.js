bb7b3318145487a11f84208af44c75cec2a2d87a
Kevin Putera <kevintiosanna@gmail.com>
2019-04-01 16:15:03 +0800

    Global styling using styled-components to remove body margin

---
+++ src/components/Global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+  }
+`;
+
+export default Global;
