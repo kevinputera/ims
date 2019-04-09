9e4576793013bd162be2702e0bce3316f2d8f7ca
Kevin Putera <kevintiosanna@gmail.com>
2019-04-09 12:11:54 +0800

    New button to activate form

---
+++ src/components/Sidebar/FormDisplayButton.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const FormDisplayButton = styled.button `
+  margin-left: auto;
+  padding: 11px;
+
+  text-align: center;
+  font-size: 20px;
+
+  border-radius: 50%;
+
+  :focus {
+    outline: none;
+  }
+`;
+
+export default FormDisplayButton;
