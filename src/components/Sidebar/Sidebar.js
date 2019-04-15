58406545f61e42e2ae7608327474605b4741a66a
Kevin Putera <kevintiosanna@gmail.com>
2019-04-15 22:47:10 +0800

    Update sidebar styling and feature

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,14 +1,8 @@
 import React from "react";
 import SidebarContainer from "./SidebarContainer";
-import SidebarMenu from "./SidebarMenu";
 import SidebarButton from "./SidebarButton";
-import FormDisplayButton from "./FormDisplayButton";
 
 class Sidebar extends React.Component {
-  clickedFormDisplayButton = () => {
-    this.props.setDisplayForm(true);
-  }
-
   render() {
     let itemList = this.props.list.map((item, index) => (
       <SidebarButton
@@ -23,10 +17,6 @@ class Sidebar extends React.Component {
 
     return (
       <SidebarContainer>
-        <SidebarMenu>
-          <span>{this.props.title}</span>
-          <FormDisplayButton onClick={this.clickedFormDisplayButton}/>
-        </SidebarMenu>
         {itemList}
       </SidebarContainer>
     );
--- src/components/Sidebar/SidebarContainer.js
+++ src/components/Sidebar/SidebarContainer.js
@@ -2,15 +2,15 @@ import styled from "styled-components";
 
 const SidebarContainer = styled.div`
   position: sticky;
+  top: 0;
 
   margin: 0;
   padding: 0;
-  width: 200px;
-  height: 100%;
+  width: 220px;
 
   background-color: white;
 
-  box-shadow: 0 0 2px grey;
+  box-shadow: 0px 0px 1px grey;
 
   overflow-x: hidden;
   overflow-y: auto;
