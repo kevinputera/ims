45e3cc496d76dcb13203cd269d2d1fd5ec4a6318
Kevin Putera <kevintiosanna@gmail.com>
2019-04-19 20:43:01 +0800

    Update Table

--- ims-app/src/components/Table/Table.js
+++ ims-app/src/components/Table/Table.js
@@ -1,4 +1,4 @@
-import config from "../../config.js";
+import config from "../../config.json";
 import React from "react";
 import TableRow from "./TableRow";
 import TableContainer from "./TableContainer";
